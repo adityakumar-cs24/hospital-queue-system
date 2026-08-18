a6e69176fd14e8f7b41e4768d6eabaa3333153f3
Aditya Kumar <aditya.cse24@gmail.com>
2026-08-18 15:36:50 +0530

    Phase 1: Backend project setup, Express server, MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hospital Appointment & Queue Management API is running...');
+});
+
+
+app.use('/api/patients', require('./routes/patientRoutes'));
+app.use('/api/doctors', require('./routes/doctorRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
+// app.use('/api/appointments', require('./routes/appointmentRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
